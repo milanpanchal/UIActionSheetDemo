be0d8a35f83753c06b3982b97e22a3985daa3c2f
milan <sam07it22@gmail.com>
2013-01-08 16:11:20 +0530

    Added actionsheet in viewDidLoadMethod

--- UIActionSheetDemo/ViewController.m
+++ UIActionSheetDemo/ViewController.m
@@ -18,12 +18,37 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    UIActionSheet *actionSheet = [[UIActionSheet alloc] initWithTitle:@"Actionsheet Demo"
+                                                             delegate:nil
+                                                    cancelButtonTitle:@"Cancel"
+                                               destructiveButtonTitle:nil
+                                                    otherButtonTitles:@"Option 1",@"Option 2", nil];
+    
+    if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone) {
+        
+        [actionSheet showFromRect:self.view.bounds inView:self.view animated:YES];
+
+        
+    } else {
+
+        // If you will un comment the following code then it will crash your app
+        
+//        [actionSheet showFromRect:self.view.bounds inView:self.view animated:YES];
+
+        UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Actionsheet will not work" message:@"If you will try to display action sheet in iPad then app will crash" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles: nil];
+        [alertView show];
+
+    }
 }
 
 - (void)viewDidUnload
 {
     [super viewDidUnload];
     // Release any retained subviews of the main view.
+    
+    
+   
 }
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
